2863a8bf285eb0bb8642200a2606506343caf174
roguelxx <xuanxin0213@gmail.com>
2021-09-13 14:18:06 +0800

    daily commit

--- ch16/16.12/Blob.h
+++ ch16/16.12/Blob.h
@@ -12,12 +12,18 @@
 using namespace std;
 
 template<typename T>
+class BlobPtr;
+
+template<typename T>
 class Blob {
+  friend class BlobPtr<T>;
  public:
   typedef T value_type;
   typedef typename vector<T>::size_type size_type;
+  // constructor
   Blob() : data(make_shared<vector<T>>()) {};
   Blob(initializer_list<T> il);
+
   size_type size() const { return data->size(); }
   bool empty() const { return data->empty(); }
   void push_back(const T &t) { data->push_back(t); }
@@ -33,14 +39,29 @@ class Blob {
 };
 
 template<typename T>
+Blob<T>::Blob(initializer_list<T> il):data(make_shared<vector<T>>(il)) {}
+
+template<typename T>
+void Blob<T>::pop_back() {
+  check(0, "pop_back() on empty Blob");
+  data->pop_back();
+}
+
+template<typename T>
 T& Blob<T>::back() {
   check(0, "back() on empty Blob");
   return data->back();
 }
 
 template<typename T>
+T& Blob<T>::operator[](size_type i){
+  check(i, "subscript out of range");
+  return (*data)[i];
+}
+
+template<typename T>
 void Blob<T>::check(std::size_t i, const string &msg) const {
-  if (i > size()) {
+  if (i >= size()) {
     throw std::out_of_range(msg);
   }
 }
--- ch16/16.12/BlobPtr.h
+++ ch16/16.12/BlobPtr.h
@@ -5,4 +5,55 @@
 #ifndef CPP_PRIME_CH16_16_12_BLOBPTR_H_
 #define CPP_PRIME_CH16_16_12_BLOBPTR_H_
 
+#include <vector>
+#include <memory>
+
+using namespace std;
+
+template<typename T>
+class Blob;
+
+template<typename T>
+class BlobPtr {
+ public:
+  typedef typename vector<T>::size_type size_type;
+  BlobPtr() : curr(0), wkpt(nullptr) {}
+  BlobPtr(Blob<T> &, size_type pos = 0);
+
+  T &deref() const;
+  BlobPtr<T> &incr();
+ private:
+  size_type curr;
+  weak_ptr<vector<T>> wkpt;
+  shared_ptr<vector<T>> check(size_type, const string &) const;
+};
+
+template<typename T>
+BlobPtr<T>::BlobPtr(Blob<T> &a, size_type pos):wkpt(a.data), curr(pos) {}
+
+template<typename T>
+shared_ptr<vector<T>> BlobPtr<T>::check(size_type i, const string &msg) const {
+  auto sp = wkpt.lock();
+  if (!sp) {
+    throw runtime_error("unbound BlobPtr.");
+  }
+  if (i >= sp->size()) {
+    throw std::out_of_range(msg);
+  }
+  return sp;
+}
+
+template<typename T>
+T &BlobPtr<T>::deref() const {
+  auto sp = check(curr, "deref() past end of BlobPtr");
+  return (*sp)[curr];
+}
+
+template<typename T>
+BlobPtr<T> &BlobPtr<T>::incr() {
+  auto ap = check(curr, "incr() past end of BlobPtr");
+  ++curr;
+  return *this;
+}
+
 #endif //CPP_PRIME_CH16_16_12_BLOBPTR_H_
